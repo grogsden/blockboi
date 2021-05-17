373f61ebb8cfa1838d76ef30d55d787720c1ab6b
grogsden <brian.stempien@protonmail.com>
2021-05-16 21:35:44 -0500

    added dropper

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,14 +7,27 @@ public class Mover : MonoBehaviour
     [SerializeField] float moveSpeed = 10f;
     void Start()
     {
-    
+        PrintInstructions();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game.");
+        Debug.Log("Use WASD keys for movement.");
+        Debug.Log("Try to win!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue,0,zValue);
     }
+
 }
